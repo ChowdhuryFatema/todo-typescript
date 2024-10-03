7be98e23e6d6f69f4d97b5be5eab77e923d75b54
ChowdhuryFatema <fatema7chowdhury.ctg@gmail.com>
2024-10-03 07:27:49 -0600

    first commit

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,21 @@
+import { FormEvent, useState } from "react";
+
+
+const AddTodo = () => {
+    const [todo, setTodo] = useState("");
+
+    const handleFormSubmit = (e:FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // handleAddToDo(todo)
+        setTodo("")
+    }
+
+    return (
+        <form onSubmit={handleFormSubmit}>
+            <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <button type="submit">Add</button>
+        </form>
+    );
+};
+
+export default AddTodo;
